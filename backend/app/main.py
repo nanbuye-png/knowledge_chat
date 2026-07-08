8d87743522df76c95f57475f46425780edc7f4a5
nanbuye-png <nanbuye34227@gmail.com>
2026-07-08 23:52:55 +0800

    Fix missing HTTPException import

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from loguru import logger
